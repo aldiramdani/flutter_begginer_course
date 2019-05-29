aa7bcff35fd17fb0408ecfe47aace2faa0fbbb96
aldi ramdani <aldiramdani8@gmail.com>
2019-05-29 11:17:57 +0700

    adding Raised Button and AlertDialog

--- lib/home.dart
+++ lib/home.dart
@@ -64,7 +64,8 @@ class Home extends StatelessWidget {
                       )),
                 ],
               ),
-              FlightImageAsset()
+              FlightImageAsset(),
+              FlighBookingButton()
             ],
           )),
     );
@@ -75,8 +76,49 @@ class Home extends StatelessWidget {
 class FlightImageAsset extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    AssetImage assetImage = AssetImage('images/images_1.jpg');
+    AssetImage assetImage = AssetImage('images/flight.png');
     Image image = Image(image: assetImage,width: 250.0,height: 250.0,);
     return Container(child: image);
   }
+}
+
+class FlighBookingButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 30.0),
+      width: 250.0,
+      height: 50.0,
+      child: RaisedButton(
+          color: Colors.deepOrange,
+          child: Text(
+              'Book Your Flight',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+                fontFamily: 'Raleway',
+                fontWeight: FontWeight.w700
+              ),
+          ),
+          elevation: 6.0,
+          onPressed: (){
+            //Action
+            bookFlight(context);
+          }),
+    );
+  }
+
+  void bookFlight(BuildContext context){
+    var alertDialog = AlertDialog(
+      title: Text('Flight Booked Successfully'),
+      content: Text('Have a pleasant flight'),
+    );
+
+    showDialog(context: context,
+    builder: (BuildContext context){
+      return alertDialog;
+    }
+    );
+  }
+
 }
